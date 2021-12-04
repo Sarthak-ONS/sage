2f136501ba5c85c93407b8473b328514ba5fe08e
Sarthak Agarwal <agarwalsarthak456@gmail.com>
2021-12-04 23:58:18 +0530

    Improved Home Screen Layour

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:sage/Providers/google_auth_provider.dart';
 
@@ -11,57 +12,86 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        floatingActionButton: FloatingActionButton(
+        floatingActionButton: FloatingActionButton.extended(
           onPressed: () {
+            //TODO: Add Search Feature
             print(
-                Provider.of<GoogleAuthpProvider>(context, listen: false).name);
+              Provider.of<GoogleAuthpProvider>(context, listen: false).name,
+            );
           },
+          backgroundColor: Colors.red,
+          label: const Text('Search'),
+          icon: const Icon(Icons.search),
         ),
         backgroundColor: Colors.black,
-        appBar: AppBar(
-          title: Image.asset(
-            "Assets/logo.png",
-            height: 45,
-            width: 45,
-            scale: 0.5,
-          ),
-          centerTitle: false,
-          backgroundColor: Colors.black,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: CircleAvatar(
-                radius: 15,
-                foregroundImage: NetworkImage(
-                  Provider.of<GoogleAuthpProvider>(context).photoUrl!,
-                ),
-              ),
-            )
-          ],
-        ),
+        appBar: buildAppBar(context),
         endDrawer: const Drawer(
           elevation: 20,
         ),
-        body: Column(
-          children: [
-            const Center(
-              child: Text('Home Page'),
-            ),
-            TextButton(
-              onPressed: () {
-                Provider.of<GoogleAuthpProvider>(context, listen: false)
-                    .logout();
-              },
-              child: const Text(
-                'Logout',
-              ),
-            )
-          ],
+        body: ListView(
+          children: const [],
         ),
       ),
     );
   }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      title: Row(
+        children: [
+          Image.asset(
+            "Assets/logo.png",
+            height: 40,
+            width: 40,
+            scale: 0.5,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            'Sage',
+            style: TextStyle(fontSize: 25, fontFamily: 'Poppins'),
+          ),
+        ],
+      ),
+      centerTitle: false,
+      backgroundColor: Colors.black,
+      actions: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            radius: 20,
+            foregroundImage: NetworkImage(
+              Provider.of<GoogleAuthpProvider>(context).photoUrl!,
+            ),
+          ),
+        )
+      ],
+    );
+  }
 }
+// Column(
+//           children: [
+//             const Center(
+//               child: Text('Home Page'),
+//             ),
+//             TextButton(
+//               onPressed: () {
+//                 Provider.of<GoogleAuthpProvider>(context, listen: false)
+//                     .logout();
+//               },
+//               child: const Text(
+//                 'Logout',
+//               ),
+//             )
+//           ],
+//         )
