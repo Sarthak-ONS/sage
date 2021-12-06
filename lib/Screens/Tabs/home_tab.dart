36ceba3daea2fa6ff1807b0627c90c5f27b3c4c6
Sarthak Agarwal <agarwalsarthak456@gmail.com>
2021-12-07 00:59:23 +0530

    Added Movied UI

--- lib/Screens/Tabs/home_tab.dart
+++ lib/Screens/Tabs/home_tab.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class HomeTab extends StatefulWidget {
   const HomeTab({Key? key}) : super(key: key);
@@ -17,48 +18,252 @@ class _HomeTabState extends State<HomeTab> {
       body: ListView(
         padding: EdgeInsets.symmetric(horizontal: 10.0),
         children: [
+          buildTrendingWidget(),
+          const SizedBox(
+            height: 15.0,
+          ),
           Container(
-            height: 200,
-            child: FutureBuilder<Object>(
-              future: FirebaseFirestore.instance.collection('Trending').get(),
-              builder: (context, AsyncSnapshot snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(
-                      color: Colors.red,
-                    ),
-                  );
-                }
-                return Container(
-                  child: CarouselSlider.builder(
-                    itemCount: snapshot.data!.docs.length,
-                    options: CarouselOptions(
-                        autoPlay: true,
-                        // aspectRatio: 2.0,
-                        enlargeCenterPage: false,
-                        viewportFraction: 1),
-                    itemBuilder: (context, index, realIdx) {
-                      return Container(
-                        height: 200,
-                        width: MediaQuery.of(context).size.width - 20,
-                        child: Text(snapshot.data!.docs.length.toString()),
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          image: DecorationImage(
-                            fit: BoxFit.fill,
-                            image: NetworkImage(
-                              snapshot.data!.docs[index]['thumbnail'],
+            height: 180,
+            color: Colors.black,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Top-Rated Movies',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Poppins',
+                    fontSize: 18,
+                  ),
+                ),
+                Container(
+                  height: 120,
+                  color: Colors.black,
+                  child: FutureBuilder(
+                    future: FirebaseFirestore.instance
+                        .collection('Movies')
+                        .where("ratings", isGreaterThan: 8.0)
+                        .get(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Shimmer.fromColors(
+                          baseColor: Colors.grey[300]!,
+                          highlightColor: Colors.grey[100]!,
+                          child: Container(
+                            height: 100,
+                            width: 180,
+                            margin: const EdgeInsets.symmetric(horizontal: 6.0),
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(15.0),
                             ),
                           ),
-                        ),
-                      );
+                        );
+                      } else if (snapshot.hasError) {
+                        return const Center(
+                          child: Text(
+                            'Please try again later',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        );
+                      } else {
+                        return ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            return BuildMovieCard(
+                              movieID: snapshot.data!.docs[index]['movieID'],
+                              movieName: snapshot.data!.docs[index]['name'],
+                              thumbnail: snapshot.data!.docs[index]
+                                  ['thumbnail'],
+                            );
+                          },
+                        );
+                      }
                     },
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            height: 180,
+            color: Colors.black,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Action Movies',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Poppins',
+                    fontSize: 18,
+                  ),
+                ),
+                Container(
+                  height: 153,
+                  color: Colors.black,
+                  child: FutureBuilder(
+                    future: FirebaseFirestore.instance
+                        .collection('Movies')
+                        .where("tags", whereIn: ['action', 'thrilling']).get(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Shimmer.fromColors(
+                          baseColor: Colors.grey[300]!,
+                          highlightColor: Colors.grey[100]!,
+                          child: Container(
+                            height: 100,
+                            width: 180,
+                            margin: const EdgeInsets.symmetric(horizontal: 6.0),
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                          ),
+                        );
+                      } else if (snapshot.hasError) {
+                        return const Center(
+                          child: Text(
+                            'Please try again later',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        );
+                      } else {
+                        return ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            print(snapshot.data!.docs.length);
+                            return GestureDetector(
+                              onTap: () {
+                                print(snapshot.data!.docs.length);
+                              },
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 12.0),
+                                child: Container(
+                                  height: 70,
+                                  width: 180,
+                                  margin: const EdgeInsets.symmetric(
+                                    horizontal: 6.0,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    //  color: Colors.black,
+                                    borderRadius: BorderRadius.circular(15.0),
+                                    image: DecorationImage(
+                                      image: NetworkImage(
+                                        snapshot.data!.docs[index]['thumbnail'],
+                                      ),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      }
+                    },
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  SizedBox buildTrendingWidget() {
+    return SizedBox(
+      height: 200,
+      child: FutureBuilder<Object>(
+        future: FirebaseFirestore.instance.collection('Trending').get(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.red,
+              ),
+            );
+          }
+          return SizedBox(
+            child: CarouselSlider.builder(
+              itemCount: snapshot.data!.docs.length,
+              options: CarouselOptions(
+                  autoPlay: true,
+                  // aspectRatio: 2.0,
+                  enlargeCenterPage: false,
+                  viewportFraction: 1),
+              itemBuilder: (context, index, realIdx) {
+                return Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width - 20,
+                  child: Text(snapshot.data!.docs.length.toString()),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.red,
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: NetworkImage(
+                        snapshot.data!.docs[index]['thumbnail'],
+                      ),
+                    ),
                   ),
                 );
               },
             ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class BuildMovieCard extends StatelessWidget {
+  const BuildMovieCard(
+      {Key? key,
+      required this.movieID,
+      required this.movieName,
+      required this.thumbnail})
+      : super(key: key);
+
+  final String movieID;
+  final String thumbnail;
+  final String movieName;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print(movieID);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12.0),
+        child: Container(
+          height: 70,
+          width: 150,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 6.0,
           ),
-        ],
+          decoration: BoxDecoration(
+            //  color: Colors.black,
+            borderRadius: BorderRadius.circular(15.0),
+            image: DecorationImage(
+              image: NetworkImage(thumbnail),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -26,87 +26,126 @@ class _HomeScreenState extends State<HomeScreen>
     );
   }
 
+  setMovieDoc() {
+    FirebaseFirestore.instance.collection('Movies').add({
+      "name": "The Bold Type",
+      "movieID": "",
+      "actors": [
+        "Warren Buffet",
+        "Bill Gates",
+        "Bill Clinton",
+      ],
+      "description":
+          "The Bold Type is an American comedy-drama television series created by Sarah Watson and produced by Universal Television for Freeform. It is inspired by the life and career of former editor-in-chief of Cosmopolitan magazine Joanna Coles, who is executive producer of the series. Filmed in Toronto, Montreal, and New York City, the series chronicles the lives of three millennial women, portrayed by Katie Stevens, Aisha Dee, and Meghann Fahy, all of whom are employed at a fictional global publication called Scarlet in New York City.",
+      "duration": 145,
+      "ratings": 8.5,
+      "tags": [
+        "Romance",
+        "Thrilling",
+        "Comedy",
+      ],
+      "link": "www.google.com",
+      "thumbnail":
+          "https://images.pexels.com/photos/9807576/pexels-photo-9807576.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      "yearOfRelease": "2017"
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {
-            //TODO: Add Search Feature
-            print(
-              Provider.of<GoogleAuthpProvider>(context, listen: false).name,
-            );
-          },
-          backgroundColor: const Color(0xffC43030),
-          label: const Text('Search'),
-          icon: const Icon(Icons.search),
-        ),
-        backgroundColor: Colors.black,
-        // appBar: buildAppBar(context),
-        endDrawer: const Drawer(
-          elevation: 20,
-        ),
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              title: Row(
-                children: [
-                  Image.asset(
-                    "Assets/logo.png",
-                    height: 40,
-                    width: 40,
-                    scale: 0.5,
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  const Text(
-                    'Sage',
-                    style: TextStyle(fontSize: 25, fontFamily: 'Poppins'),
-                  ),
-                ],
-              ),
-              backgroundColor: Colors.black,
-              // pinned: true,
-              snap: true,
-              floating: true,
-              pinned: true,
-              expandedHeight: 130.0,
-              actions: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CircleAvatar(
-                    radius: 20,
-                    foregroundImage: NetworkImage(
-                      Provider.of<GoogleAuthpProvider>(context).photoUrl!,
+      child: Center(
+        child: Scaffold(
+          floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {
+              //TODO: Add Search Feature
+              setMovieDoc();
+              print(
+                Provider.of<GoogleAuthpProvider>(context, listen: false).name,
+              );
+            },
+            backgroundColor: const Color(0xffC43030),
+            label: const Text('Search'),
+            icon: const Icon(Icons.search),
+          ),
+          backgroundColor: Colors.black,
+          // appBar: buildAppBar(context),
+          endDrawer: const Drawer(
+            elevation: 20,
+          ),
+          body: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                title: Row(
+                  children: [
+                    Image.asset(
+                      "Assets/logo.png",
+                      height: 30,
+                      width: 30,
+                      scale: 0.5,
                     ),
-                  ),
-                )
-              ],
-              bottom: TabBar(
-                indicatorColor: Colors.red,
-                indicatorPadding: EdgeInsets.symmetric(horizontal: 20.0),
-                indicatorSize: TabBarIndicatorSize.tab,
-                tabs: const [
-                  Tab(text: 'Home'),
-                  Tab(text: 'TV Shows'),
-                  Tab(text: 'Movies'),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Text(
+                      'S',
+                      style: TextStyle(fontSize: 30, fontFamily: 'Poppins'),
+                    ),
+                    const Text(
+                      'age',
+                      style: TextStyle(fontSize: 20, fontFamily: 'Poppins'),
+                    ),
+                  ],
+                ),
+                backgroundColor: Colors.black,
+                // pinned: true,
+                snap: true,
+                floating: true,
+                pinned: true,
+                expandedHeight: 130.0,
+                actions: [
+                  GestureDetector(
+                    onTap: () {
+                      Scaffold.of(context).openEndDrawer();
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        radius: 20,
+                        foregroundImage: NetworkImage(
+                          Provider.of<GoogleAuthpProvider>(context).photoUrl!,
+                        ),
+                      ),
+                    ),
+                  )
                 ],
-                controller: controller,
+                bottom: TabBar(
+                  indicatorColor: const Color(0xffC43030),
+                  indicatorSize: TabBarIndicatorSize.tab,
+                  labelStyle: TextStyle(fontSize: 14),
+                  isScrollable: false,
+                  physics: BouncingScrollPhysics(),
+                  tabs: const [
+                    Tab(text: 'Home'),
+                    Tab(text: 'TV Shows'),
+                    Tab(text: 'Movies'),
+                  ],
+                  controller: controller,
+                ),
               ),
-            ),
-            // SliverList(
-            SliverFillRemaining(
-              child: TabBarView(
-                controller: controller,
-                children: const [
-                  HomeTab(),
-                  Center(child: Text("Tab two")),
-                  Center(child: Text("Tab three")),
-                ],
-              ),
-            )
-          ],
+              // SliverList(
+              SliverFillRemaining(
+                child: TabBarView(
+                  controller: controller,
+                  children: const [
+                    HomeTab(),
+                    Center(child: Text("Tab two")),
+                    Center(child: Text("Tab three")),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
